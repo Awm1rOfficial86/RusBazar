89be1856697e2c51b95288eb56b429ed3aa232f0
Awm1rOfficial86 <166319387+Awm1rOfficial86@users.noreply.github.com>
2024-09-07 00:04:17 +0330

    fix error

--- BotTel.V.1.1.py
+++ BotTel.V.1.1.py
@@ -2,9 +2,16 @@ from telebot import TeleBot
 from telebot import types
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 from telebot.types import KeyboardButton, ReplyKeyboardMarkup
-from config import channel, TOKEN
 import time
 from telebot.handler_backends import StatesGroup, State
+
+
+TOKEN: str  =  "7500595361:AAE9PbI6fZ4gX81alp2p8fAVAur_zoeJU0M"
+
+
+channel: list[str] = ['@lovemckIR' , '@rednews2022' , '@TNT2024TNT' ,'@iran_russ2024' ,'@niazmndiha_2024_rus']
+
+
 
 mychatid = 6636146690
 bot = TeleBot(token=TOKEN)
