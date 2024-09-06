bb4275c72e7f37093d727842130da8b31d10d264
Awm1rOfficial86 <166319387+Awm1rOfficial86@users.noreply.github.com>
2024-09-06 23:45:00 +0330

    Fix

---
+++ config.py
@@ -0,0 +1,9 @@
+
+TOKEN: str  =  "7500595361:AAE9PbI6fZ4gX81alp2p8fAVAur_zoeJU0M"
+
+
+channel: list[str] = ['@lovemckIR' , '@rednews2022' , '@TNT2024TNT' ,'@iran_russ2024' ,'@niazmndiha_2024_rus']
+
+
+
+admins = [6636146690 , 963475140]
